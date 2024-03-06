5b532128451d50e622fbcf38d353537a69a6feeb
Danylo Korsun <dakorsun@gmail.com>
2024-03-06 23:41:03 +0200

    feat(three): implemented simple canvas scene
    - Box with Plane
    - OrbitConrols and stars
    - basic usage of cannon physics

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import * as THREE from 'three';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+import { Physics, useBox, usePlane } from '@react-three/cannon';
+
+function Box() {
+  const position: [number, number, number] = [0, 2, 0];
+  const [ref, api] = useBox(() => ({ mass: 1, position }), useRef<THREE.Mesh>(null));
+  return (
+    <mesh
+      onClick={() => {
+        api.velocity.set(0, 2, 0);
+      }}
+      ref={ref}
+      position={position}
+    >
+      <boxGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
+}
+
+function Plane() {
+  const rotation: [number, number, number] = [-Math.PI / 2, 0, 0];
+  const [ref] = usePlane(
+    () => ({
+      rotation,
+    }),
+    useRef<THREE.Mesh>(null),
+  );
+  return (
+    <mesh ref={ref} rotation={rotation}>
+      <planeGeometry attach="geometry" args={[50, 50]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas camera={{ position: [5, 5, 5], fov: 100 }}>
+      <OrbitControls />
+      <Stars />
+      <ambientLight intensity={0.5} />
+      <directionalLight color="white" position={[10, 15, 10]} />
+      <Physics>
+        <Box />
+        <Plane />
+      </Physics>
+    </Canvas>
   );
 }
 
