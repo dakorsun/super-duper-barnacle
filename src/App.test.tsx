780d4288533b07b29ed32c8f9aa61a4021d86858
Danylo Korsun <dakorsun@gmail.com>
2024-03-06 17:03:05 +0200

    fix(testing): configured jest globals through the CommonJS

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+const { test, expect } = require('@jest/globals');
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
